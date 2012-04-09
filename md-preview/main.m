203412941c9cfe213453329ceb973d6b24f5b4ee
Danny Greg <danny@github.com>
2012-04-09 20:49:36 +0100

    Initial project template and git ignore.

---
+++ md-preview/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  md-preview
+//
+//  Created by Danny Greg on 09/04/2012.
+//  Copyright (c) 2012 GitHub. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
